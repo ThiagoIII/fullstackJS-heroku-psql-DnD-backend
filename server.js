7868184fe5b45599adea7df1dcfe6a43f96fb45c
ThiagoIII <thiagoiii@hotmail.com>
2020-10-10 16:49:02 -0300

    fix

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const cors = require('cors')
 
 const app = express()
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+});
+
 app.use(cors())
 app.use(express.json());
 app.use(routes)
