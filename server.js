ab7182be1038809fcef914acf74bcb114cf8f60d
ThiagoIII <thiagoiii@hotmail.com>
2020-10-10 17:54:56 -0300

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const app = express()
 
 
-app.use(cors())
+app.use(cors({origin: 'https://salty-thicket-65996.herokuapp.com/'}))
 app.use(express.json());
 app.use(routes)
 
