d508d3edc1157f2ad15c1af3f271b60988eee598
ThiagoIII <thiagoiii@hotmail.com>
2020-10-10 16:10:46 -0300

    added db

--- controllers/RegisterController.js
+++ controllers/RegisterController.js
@@ -1,28 +1,25 @@
 const db = require('../database/connection')
-const crypto = require('crypto'); 
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 module.exports = {
 
 	async index(request, response) {
-		let { age, name, birthday, password } = request.body
-		if (!age || !name || !birthday || !password) {
+		let { name, email, password } = request.body
+		if (!name || !email || !password) {
 			return response.status(400).json('incorrect form submission, some input is empty');
 		}
 		
-		const id = crypto.randomBytes(4).toString('HEX');
 		const salt = bcrypt.genSaltSync(saltRounds);
 		const hash = bcrypt.hashSync(password,salt);
 
 		try {
 			const data = await db('users').insert({
-				age,
 				name,
-				birthday,
-				password: hash
+				email,
+				hash
 			})
-			return response.json('User registered with success')
+			return response.json(data)
 		} catch (error) {
 			return response.status(400).json('unable to register', error)
 		}
