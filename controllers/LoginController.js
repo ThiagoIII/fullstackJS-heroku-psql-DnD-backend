8ed546d194f64e8d3d1aaed5b1c2f8965313e6ec
ThiagoIII <thiagoiii@hotmail.com>
2020-10-13 13:12:29 -0300

    db configuration

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -8,11 +8,12 @@ module.exports = {
 		let { name,  password } = request.body
 		try {
 			const dataLogin = await db.select('*').from('users').where({name: name})
-			const dataLoginArr = dataLogin.data
+			const dataLoginArr = await dataLogin.json()
+			const loginArr = await dataLoginArr.data
 			/* const hash1 = await dataLogin */
 			/* const isValid = bcrypt.compareSync(password, hash1) 
 			isValid ? response.json(dataLogin.data[0]) : response.status(400).json('wrong password') */
-			return response.json(dataLoginArr)
+			return response.json(loginArr)
 			
 		} catch (error) {
 			return response
