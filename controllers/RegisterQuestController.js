33940e5403334c9d3e650359b648f34f923331e0
ThiagoIII <thiagoiii@hotmail.com>
2020-11-16 14:43:08 -0300

    added register quest controller and some validation for quests

--- controllers/RegisterQuestController.js
+++ controllers/RegisterQuestController.js
@@ -8,16 +8,19 @@ module.exports = {
 			return response
 		}
 		try {
-			await db(`quests`)
-				.insert({
-					id: id,
-					questname: questName,
-					quest: questHistory
-				})
-			let newQuest = await db.select('*')
-				.from('quests')
-				.where({questname: questName})	
-			return response.json(newQuest)
+			let newQuest = await db.select('*').from('quests').where({questname: questName})
+			if(newQuest.length > 0) {
+				return response.status(400).json('Quest name already exists')
+			} else {
+				await db(`quests`)
+						.insert({
+							id: id,
+							questname: questName,
+							quest: questHistory
+						})
+				newQuest = await db.select('*').from('quests').where({questname: questName})
+				return response.json(newQuest)
+			}
 		} catch (error) {
 			return response.status(400).json(`Not possible to register: ${error}`) 
 		}
