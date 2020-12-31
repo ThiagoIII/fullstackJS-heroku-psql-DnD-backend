85a17be1e3e0ee97a73976c83e04f7bc13cdcfad
ThiagoIII <thiagoiii@hotmail.com>
2020-12-31 11:56:39 -0300

    changing some validation code

--- controllers/RegisterChar.js
+++ controllers/RegisterChar.js
@@ -1,27 +1,41 @@
 const db = require('../database/connection')
 
 module.exports = {
-
-	async index(request, response) {
-		let { charname, charhistory, id } = request.body
-		if (!id || !charname || !charhistory) {
-			return response.status(400).json('Some of the inputs are empty, please, re-check and try again!') 
-		}
-		let newChar = await db.select('*').from('quests').where({charname: charname})
-		if(newChar.length > 0) {
-			return response.status(400).json('Uh! Sorry but it seems that this super epic hero or not char name its already taken! Would you be so kind to give it another go ? Think Chuck Norris + Agamemnon, like Chagamem Nonnis!')
-		} 
-		try {
-			await db(`chars`)
-					.insert({
-						id: id,
-						charname: charname,
-						charhistory: charhistory
-					})
-			newChar = await db.select('*').from('chars').where({charhistory: charhistory})	
-			return response.json(newChar)
-		} catch (error) {
-			return response.status(400).json(`Not possible to register: ${error}`) 
-		}
-	}
-};
+    async index(request, response) {
+        let { charname, charhistory, id } = request.body
+        if (!id || !charname || !charhistory) {
+            return response
+                .status(400)
+                .json(
+                    'Some of the inputs are empty, please, re-check and try again!'
+                )
+        }
+        let newChar = await db
+            .select('*')
+            .from('quests')
+            .where({ charname: charname })
+        if (newChar.length > 0) {
+            return response
+                .status(400)
+                .json(
+                    'Uh! Sorry but it seems that this super epic hero or not char name its already taken! Would you be so kind to give it another go ? Think Chuck Norris + Agamemnon, like Chagamem Nonnis!'
+                )
+        }
+        try {
+            await db(`chars`).insert({
+                id: id,
+                charname: charname,
+                charhistory: charhistory
+            })
+            newChar = await db
+                .select('*')
+                .from('chars')
+                .where({ charhistory: charhistory })
+            return response.json(newChar)
+        } catch (error) {
+            return response
+                .status(400)
+                .json(`Not possible to register: ${error}`)
+        }
+    }
+}
--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const DiscoverCharsAndAdvs = require('./controllers/DiscoverCharsAndAdvs');
-const FetchUserDataController = require('./controllers/FetchUserDataController');
-const LoginController = require('./controllers/LoginController');
-const RegisterChar = require('./controllers/RegisterChar');
-const RegisterQuest = require('./controllers/RegisterQuestController');
-const RegisterController = require('./controllers/RegisterController');
+const express = require('express')
+const DiscoverCharsAndAdvs = require('./controllers/DiscoverCharsAndAdvs')
+const FetchUserDataController = require('./controllers/FetchUserDataController')
+const LoginController = require('./controllers/LoginController')
+const RegisterChar = require('./controllers/RegisterChar')
+const RegisterQuest = require('./controllers/RegisterQuestController')
+const RegisterController = require('./controllers/RegisterController')
 
 const router = express.Router()
 
@@ -13,8 +13,7 @@ router.get('/discoverCharsAndAdvs', DiscoverCharsAndAdvs.index)
 router.post('/fetchUserData', FetchUserDataController.index)
 router.post('/register', RegisterController.index)
 router.post('/login', LoginController.index)
-router.post('/registerChar', RegisterChar.index)
-router.post('/registerQuest', RegisterQuest.index)
-
+router.post('/registerchar', RegisterChar.index)
+router.post('/registerquest', RegisterQuest.index)
 
 module.exports = router
